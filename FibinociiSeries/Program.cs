d7ac20b6514928ac34c3201de48f6480ffea8c6c
Anandhakrishnan1 <anandhakrishnana1@gmail.com>
2024-06-04 17:30:35 +0530

    Fibinocii series

---
+++ FibinociiSeries/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        var limit =  Convert.ToInt32(Console.ReadLine());
+        int a = 0, b = 1, c = 0;
+
+        Console.WriteLine(string.Format("Your fibinocii series on limit {0} is below; \n", limit));
+        for (int i = 0; i < limit; i++)
+        {
+            Console.WriteLine(c);
+            a = b;
+            b = c;
+            c = a + b;
+        }
+    }
+}
+
