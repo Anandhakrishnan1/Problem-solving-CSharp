3d77a2dcd7e97bbca52195be1964435657bcd9ab
Anandhan-anil <48061806+Anandhakrishnan1@users.noreply.github.com>
2024-07-28 14:05:14 +0530

    Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        var isPalindrome = false;
+        string orginalText = Console.ReadLine().ToLower();
+        var length = orginalText.Length;
+        for (var i = 0; i < orginalText.Length; i++)
+        {
+            if (orginalText[i] != orginalText[length - 1])
+            {
+                break;
+            }
+            length--;
+            isPalindrome = true;
+        }
+        if (isPalindrome) { Console.WriteLine(orginalText + " is palindrome"); }
+        else { Console.WriteLine(orginalText + " is not palindrome"); }
+    }
+}
